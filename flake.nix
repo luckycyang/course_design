c7134bbf54547a654075be60fdf4c6d2be21126a
luckycyang <kfove.liang@protonmail.com>
2025-05-23 22:29:00 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowBroken = true;
+        };
+      in {
+        # flake contents here
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cabal-install
+              (haskellPackages.ghcWithPackages (p:
+                with p; [
+                  alex
+                  happy
+                ]))
+            ];
+          };
+        };
+      }
+    );
+}
